340f078e247ca3af5f09d861eb9b68dbab4a9703
Jake Cooper <jakec729@mac.com>
2016-02-27 17:41:04 -0500

    Removed movie examples

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,14 +12,9 @@ Route::group(['middleware' => 'web'], function () {
     Route::get('/users', 'UserController@index');
     Route::get('/roles', 'RoleController@index');
 
-Route::get('/files/import', 'FileController@import');
+    Route::get('/files/import', 'FileController@import');
     Route::post('/files/review', 'FileController@review');
     Route::post('/files', 'FileController@store');
-
-    Route::post('/movies/{id}/comments/create', 'MovieController@addComment');
-    Route::post('/movies/form', 'MovieController@indexForm');
-    Route::get('/movies/assign', 'MovieController@assignMoviesToUsers');
-    Route::resource('movies', 'MovieController');
 
     Route::get('/applications', 'ApplicationController@index');
     Route::get('/applications/download', 'ApplicationController@downloadCSV');
